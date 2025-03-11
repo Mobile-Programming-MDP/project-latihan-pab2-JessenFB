a5e48921a259d55614dd45f4187b88614631d378
JessenFB <jessenfallikbemi_2327250065@mhs.mdp.ac.id>
2025-03-11 18:58:20 +0700

    p

--- movie/lib/screens/detail_screen.dart
+++ movie/lib/screens/detail_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pilem/models/movie.dart';
+import 'package:movie/models/movie.dart';
 class DetailScreen extends StatelessWidget {
 final Movie movie;
 const DetailScreen({super.key, required this.movie});
