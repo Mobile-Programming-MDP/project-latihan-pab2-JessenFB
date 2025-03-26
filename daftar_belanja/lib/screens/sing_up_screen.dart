f89c51160560710a35f8bb656ca18594e9ad4a6b
JessenFB <161033437+JessenFB@users.noreply.github.com>
2025-03-26 17:31:08 +0700

    p

---
+++ daftar_belanja/lib/screens/sing_up_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/foundation.dart';
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
